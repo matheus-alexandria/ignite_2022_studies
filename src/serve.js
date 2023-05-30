a7008a648fec6a4554bbf7f5b3abb64a6eb9fe89
Matchobas <maalexandria@hotmail.com>
2023-05-29 22:30:24 -0300

    Creating scaleble routes without ifs

--- src/serve.js
+++ src/serve.js
@@ -1,32 +1,17 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select('users');
-    return res
-    .end(JSON.stringify(users));
-  }
+  const route = routes.find((r) => {
+    return r.method === method && r.path === url
+  });
 
-  if (method === "POST" && url === "/users") {
-    const { name, email } = req.body;
-    const user = {
-      id: 1,
-      name,
-      email
-    };
-
-    database.insert('users', user);
-
-    return res.writeHead(201).end();
-  }
+  if (route) return route.handler(req, res);
 
   return res.writeHead(404).end();
 });
